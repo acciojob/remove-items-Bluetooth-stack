179f6a7bedf1a0f44792cbb04fed85899f39d823
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-13 23:54:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const drop = document.querySelector("#colorSelect");
+const button = document.querySelector("#btn");
+
+button.addEventListener("click",()=>{
+	if(drop.hasChildNodes()){
+		let child = drop.firstElementChild;
+		drop.removeChild(child);
+	}
+})
